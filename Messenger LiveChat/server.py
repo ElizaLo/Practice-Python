af7c63aa4158f7caadb0a474be8a02e2e4373d43
Yelyzaveta L <36056528+ElizaLo@users.noreply.github.com>
2020-03-26 01:20:48 +0200

    Update server.py

--- Messenger LiveChat/server.py
+++ Messenger LiveChat/server.py
@@ -1,25 +1,96 @@
-from flask import Flask
+import time
+from flask import Flask, request, abort
 from datetime import datetime
-from datetime import date
-from datetime import time
+# from datetime import date
+
 
 app = Flask(__name__)
 
-@app.route("/")
+
+messages = [
+    {'username': 'Li', 'text': 'Hello', 'time': 0.0}
+]
+
+users = {
+    'Li': '123456'
+}
+
+
+@app.route("/") # decorator
 def hello():
     return "Hello everybody! " \
            "\n\n Welcome to LiveChat"
 
+
 @app.route("/status")
 def status():
-    curr_date = date.today()
-    curr_time = str(datetime.time(datetime.now()))
+    #curr_date = date.today()
+    datetime.now().strftime('%Y/%m/%d %H:%M:%S')
 
     return {
         'status': True,
         'name': 'LiveChat',
-        'date': curr_date,
-        'time': curr_time,
+        #'date': curr_date,
+        'time': datetime.now().strftime('%Y/%m/%d %H:%M:%S'),
     }
+
+
+@app.route("/send", methods = ['POST'])
+def send():
+    '''
+    принимаем JSON
+    {
+        "username": str,
+        "password": str,
+        "text": str,
+    }
+    :return: JSON {"ok": true}
+    '''
+    username = request.json['username']
+    password = request.json['password']
+
+    if username in users: # зарегистрированный пользователь
+        if password != users[username]: # авторизуем
+            return abort(401)
+    else: # новй пользователь
+        users[username] = password # регистрируем
+
+    text = request.json['text']
+    current_time = time.time()
+    message = {'username': username, 'text': text, 'time': current_time}
+    messages.append(message)
+
+    print(messages)
+
+    return {"ok": True}
+
+
+@app.route("/messages")
+def messages_view():
+    '''
+    принимаем ?after=float
+    :return: JSON   {
+        "messages": [
+            {"username": str, "text": str, "time": float},
+            ...
+        ]
+    }
+    '''
+    after =  float(request.args.get('after'))
+
+    # filtered_messages = []
+    # for message in messages:
+    #     if message['time'] > after:
+    #         filtered_messages.append(message)
+
+    # list comprehension
+    filtered_messages = [message for message in messages if message['time'] > after]
+
+    return {
+        'messages': filtered_messages
+    }
+
+
+
 
 app.run()
