301af279537ac66d26aeb36d608e791aa40ac3ce
Yelyzaveta L <36056528+ElizaLo@users.noreply.github.com>
2020-04-20 20:44:28 +0300

    Create emailed.py

---
+++ Python for Everybody/databases/emailed/emailed.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('''
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
+
+fname = input('Enter file name: ')
+if (len(fname) < 1): fname = 'mbox.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith('From') or line.startswith('From:'): continue
+    pieces = line.split()
+    domain = pieces[1].split('@')
+    org = domain[1]
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES (?, 1)''', (org,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (org,))
+    conn.commit()
+
+# https://www.sqlite.org/lang_select.html
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
