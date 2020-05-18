584410af204aef092719c2f8f53b1f3a4dca4e8b
Yelyzaveta L <36056528+ElizaLo@users.noreply.github.com>
2020-05-18 19:32:28 +0300

    Create np_arrays.py

---
+++ DataCamp/Writing Efficient Python Code/Foundations for efficiencies/np_arrays.py
@@ -0,0 +1,13 @@
+# Print second row of nums
+print(nums[0, :])
+
+# Print all elements of nums that are greater than six
+print(nums[nums > 6])
+
+# Double every element of nums
+nums_dbl = nums * 2
+print(nums_dbl)
+
+# Replace the third column of nums
+nums[:,2] = nums[:,2] + 1
+print(nums)
